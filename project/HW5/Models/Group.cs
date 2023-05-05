7c92de6d6f95a8bf49cc0321085577cb847c1613
CharNoir <d.buryak2002@gmail.com>
2023-05-05 10:04:47 +0300

    add models

---
+++ project/HW5/Models/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace HW5.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Temp { get; set; }
+    }
+}
---
+++ project/HW5/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace HW5.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public int AnalysisId { get; set; }
+    }
+}
