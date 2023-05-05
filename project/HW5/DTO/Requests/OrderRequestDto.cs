6f47db0075f748dc8803c510b700536bf754107d
CharNoir <d.buryak2002@gmail.com>
2023-05-05 10:04:40 +0300

    add dto

---
+++ project/HW5/DTO/Requests/OrderRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HW5.DTO.Requests
+{
+    public class OrderRequestDto
+    {
+        public DateTime DateTime { get; set; }
+        public int AnalysisId { get; set; }
+    }
+}
---
+++ project/HW5/DTO/Responses/OrderFullResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HW5.DTO.Responses
+{
+    public class OrderFullResponseDto
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public AnalysisResponseDto Analysis { get; set; }
+    }
+}
