b655c95df106124331f366ed0f21ffa4df6be5e1
CharNoir <d.buryak2002@gmail.com>
2023-05-07 16:24:09 +0300

    add switch for preferred methode

--- project/HW5/Program.cs
+++ project/HW5/Program.cs
@@ -1,16 +1,40 @@
 using HW5.DAO;
 using HW5.DAO.Implementation;
+using HW5.Models;
 using HW5.Services;
 using HW5.Services.Implementation;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IOrderDao, OrderDao>();
+
+string connectionMethod = (builder.Configuration["PrefferedDataBaseConnectionMethode"] ?? "EF").ToUpper().Trim();
+switch (connectionMethod)
+{
+    case "EF":
+        {
+            builder.Services.AddTransient<IAnalysisDao, AnalysisEfDao>();
+            builder.Services.AddTransient<IOrderDao, OrderEfDao>();
+            break;
+        }
+    case "ADO":
+        {
+            builder.Services.AddTransient<IAnalysisDao, AnalysisAdoDao>();
+            builder.Services.AddTransient<IOrderDao, OrderAdoDao>();
+            break;
+        }
+    default:
+        {
+            throw new Exception("Invalid preferred database connection method");
+        }
+}
+
 builder.Services.AddTransient<IOrderService, OrderService>();
-builder.Services.AddTransient<IAnalysisDao, AnalysisDao>();
 builder.Services.AddTransient<IAnalysisService, AnalysisService>();
+builder.Services.AddDbContext<OrdersDBContext>(
+        options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
 var app = builder.Build();
 
